d485fa6421293185911a0c02c5152d05c8becfb0
Atharv Gupta <atharvgupta6027@gmail.com>
2026-07-03 00:37:34 +0530

    Time: 11 ms (15.75%), Space: 53.7 MB (85.86%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int numEnclaves(int[][] board) {
+        int m=board.length;
+        int n=board[0].length;
+
+        Queue<int []> q=new LinkedList<>();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1){
+                    if(i==0 || j== 0 || i==m-1 || j==n-1){
+                        q.add(new int[]{i,j});
+                        board[i][j]=-1;
+                    }
+                }
+            }
+        }
+        int directions[][]={{-1,0},{0,-1},{1,0},{0,1}};
+
+        while(!q.isEmpty()){
+            int curr[]=q.remove();
+
+            for(int dir[]:directions){
+                int nr=dir[0]+curr[0];
+                int nc=dir[1]+curr[1];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n){
+                    if(board[nr][nc]==1){
+                        board[nr][nc]=-1;
+                        q.add(new int[]{nr,nc});
+                    }
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
